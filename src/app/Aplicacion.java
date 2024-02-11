1d3b60057774eed468e48108337fbcd80fd873c3
Timur Bogach <Timurnator_@outlook.com>
2024-02-11 15:50:16 +0100

    v0.0

---
+++ src/app/Aplicacion.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package app;
+
+/**
+ * @author Timur Bogach
+ * @date 11 feb 2024
+ * 
+ */
+
+public class Aplicacion {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/leer/Leer.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package leer;
+
+/**
+ * @author Timur Bogach
+ * @date 11 feb 2024
+ * 
+ */
+
+public class Leer {
+
+}
